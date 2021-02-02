90a743ec9c1cc7f72d95e8fbfcccc9ecca4d603a
iamWebLuk <lukas.weber@edu.fh-joanneum.at>
2021-02-02 20:42:49 +0100

    §

---
+++ main.js
@@ -0,0 +1,55 @@
+
+
+/*const menuBtn = document.querySelector('.navbar-toggler-icon');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+} else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+}
+});*/
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+/*
+const menuBtn = document.querySelector('.navbar-toggler');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
+  */
